96fc83721f3ea323995434a02d29198f8baaf16a
Tony Ganchev <tony.ganchev@gmail.com>
2025-10-26 16:39:13 +0200

    devops: aaply eslint rules to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,32 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025-present Tony Ganchev and contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
-import stylistic from "@stylistic/eslint-plugin"
+import stylistic from "@stylistic/eslint-plugin";
 import { defineConfig } from "eslint/config";
 import eslintPlugin from "eslint-plugin-eslint-plugin";
 import jsdoc from "eslint-plugin-jsdoc";
@@ -21,6 +45,8 @@ const compat = new FlatCompat({
 const jsRules = {
     extends: [
         ...compat.extends("eslint:recommended"),
+        eslintPlugin.configs.recommended,
+        jsdoc.configs["flat/recommended"],
     ],
     plugins: {
         "eslint-plugin": eslintPlugin,
@@ -186,8 +212,22 @@ const jsRules = {
 };
 
 export default defineConfig([
-    jsdoc.configs["flat/recommended"],
-    eslintPlugin.configs.recommended,
+    {
+        ignores: [
+            "coverage/**",
+            ".vscode/**"
+        ]
+    },
+    {
+        files: ["**/*.mjs"],
+        languageOptions: {
+            sourceType: "module",
+            globals: {
+                ...globals.node,
+            },
+        },
+        ...jsRules,
+    },
     {
         files: ["lib/**/*.js"],
         languageOptions: {

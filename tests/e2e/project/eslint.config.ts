6d8d8b42a737ac9b758a0a0b34cfe160f00e576a
Tony Ganchev <tony.ganchev@gmail.com>
2026-04-03 14:36:19 +0300

    test: E2E tests for CSS and MD for ESLint 9/10 (#179)

--- tests/e2e/project/eslint.config.ts
+++ tests/e2e/project/eslint.config.ts
@@ -3,6 +3,8 @@
 
 import { defineConfig } from "eslint/config";
 import plugin, { HeaderOptions, HeaderRuleConfig } from "@tony.ganchev/eslint-plugin-header";
+import css from "@eslint/css";
+import markdown from "@eslint/markdown";
 
 export default defineConfig([
     {
@@ -19,6 +21,56 @@ export default defineConfig([
                         lines: [
                             " Copyright 1985",
                             " Tony Ganchev"
+                        ]
+                    },
+                    lineEndings: "os",
+                    trailingEmptyLines: {
+                        minimum: 2
+                    }
+                } as HeaderOptions
+            ] as HeaderRuleConfig
+        }
+    },
+    {
+        files: ["*.css"],
+        plugins: {
+            "@tony.ganchev/header": plugin,
+            css
+        },
+        language: "css/css",
+        rules: {
+            "@tony.ganchev/header/header": [
+                "error",
+                {
+                    header: {
+                        commentType: "block",
+                        lines: [
+                            " Copyright 1985 ",
+                        ]
+                    },
+                    lineEndings: "os",
+                    trailingEmptyLines: {
+                        minimum: 2
+                    }
+                } as HeaderOptions
+            ] as HeaderRuleConfig
+        }
+    },
+    {
+        files: ["*.md"],
+        plugins: {
+            "@tony.ganchev/header": plugin,
+            markdown
+        },
+        language: "markdown/commonmark",
+        rules: {
+            "@tony.ganchev/header/header": [
+                "error",
+                {
+                    header: {
+                        commentType: "block",
+                        lines: [
+                            " Copyright 1985 ",
                         ]
                     },
                     lineEndings: "os",
